22b98e40b419dbb1a1d30578e4802ee023c405cd
archi <archi@MacBook-archi.local>
2016-03-20 21:46:32 +0600

    Add buttons and actions

--- Imagedit/ViewController.h
+++ Imagedit/ViewController.h
@@ -10,6 +10,12 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic) IBOutlet UIImageView * m_pImageView;
+
+- (IBAction)loadImage:(id)sender;
+- (IBAction)rotateImage:(id)sender;
+- (IBAction)invertColors:(id)sender;
+- (IBAction)mirrorImage:(id)sender;
 
 @end
 
--- Imagedit/ViewController.m
+++ Imagedit/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationBarDelegate>
 
 @end
 
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    [m_pImageView ];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +25,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)loadImage:(id)sender {
+    UIImagePickerController *pickerC =
+    [[UIImagePickerController alloc] init];
+    pickerC.delegate = self;
+    [self presentViewController:pickerC animated:YES completion:nil];
+}
+
+- (IBAction)rotateImage:(id)sender {
+    _m_pImageView.transform = CGAffineTransformMakeRotation(M_PI_2);
+}
+
+- (UIImage *)convertImageToGrayScale:(UIImage *)image {
+    // Create image rectangle with current image width/height
+    CGRect imageRect = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    // Grayscale color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    
+    // Create bitmap content with current image size and grayscale colorspace
+    CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, 0, colorSpace, kCGImageAlphaNone);
+    
+    // Draw image into current context, with specified rectangle
+    // using previously defined context (with grayscale colorspace)
+    CGContextDrawImage(context, imageRect, [image CGImage]);
+    
+    // Create bitmap image info from pixel data in current context
+    CGImageRef imageRef = CGBitmapContextCreateImage(context);
+    
+    // Create a new UIImage object
+    UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+    
+    // Release colorspace, context and bitmap information
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    CFRelease(imageRef);
+    
+    // Return the new grayscale image
+    return newImage;
+}
+
+- (IBAction)invertColors:(id)sender {
+    _m_pImageView.image = [self convertImageToGrayScale:_m_pImageView.image];
+}
+
+- (IBAction)mirrorImage:(id)sender {
+    _m_pImageView.transform = CGAffineTransformMakeScale(-1, 1);
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"%@", info);
+    
+    _m_pImageView.image =
+    [info objectForKey:UIImagePickerControllerOriginalImage];
+}
+
+UICollectionView
+
+- (void)imagePickerControllerDidCancel:
+(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 @end
